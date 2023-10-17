763a2fdc84e028a54ea0035aeb5889fe895f4b80
liammclay <mclay.liam.24@c-dh.org>
2023-10-17 12:29:17 -0500

    prgms

---
+++ 020_LoanCalculator/LoanCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LoanCalculator 
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What is your principal balance? ");
+        int pb = scan.nextInt();
+        
+        System.out.print("What is your annual interest rate? ");
+        double air = scan.nextDouble();
+
+        System.out.print("how many months are left on you loan plan? ");
+        int months = scan.nextInt();
+
+        double monthlyInterest = pb*(air/months);
+        double monthlyPayment = pb/months+monthlyInterest;
+
+        System.out.println("your monthly interest is "+monthlyInterest+".");
+        System.out.println("your monthly payments are "+monthlyPayment+".");
+
+    }
+    
+}
+/*used scanner to have user input values
+ * did the calculations with the variables i got from the user.
+ * printed the results
+ */
---
+++ 021_CarBuilderOne/CarBuilderOne.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CarBuilderOne 
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("what year are you looking for? ");
+        int year = scan.nextInt();
+
+        System.out.print("what mileage are you looking for? ");
+        int miles = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("what make are you looking for? ");
+        String make = scan.nextLine();
+
+        System.out.print("what model are you looking for? ");
+        String model = scan.nextLine();
+
+        Car car = new Car(year, miles, make, model);
+
+        System.out.println("Great, we found a "+car.year+" "+car.manufacturerName+" "+car.modelName+" with "+car.milesDriven+" miles on it");
+
+
+    }
+    
+}
+
+/*used scanner to get the strings and ints from user and stored them in varaibaes
+ * then put the vriables in the car object
+ * used the new object to print out the car info
+ */
