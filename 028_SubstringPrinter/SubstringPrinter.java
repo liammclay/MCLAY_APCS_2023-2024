cb51927e3f2984dd44cd17ef65762f774aeb99a2
liammclay <mclay.liam.24@c-dh.org>
2023-10-31 14:01:00 -0500

    kai boden is bad at programming; L.

---
+++ 028_SubstringPrinter/SubstringPrinter.java
@@ -0,0 +1,15 @@
+public class SubstringPrinter {
+    public static void main(String[] args){
+        String string = "The quick brown fox jumps over the lazy dog.";
+
+        System.out.println(string.substring(40, 43));
+        System.out.println(string.substring(16, 19));
+        System.out.println(string.substring(37, 39));
+        System.out.println(string.substring(17, 23));
+        System.out.println(string.substring(42, 43));
+    }
+    
+}
+/*put sentence in a string
+ * used substring method inside println method to print the desired letters/words
+ */
