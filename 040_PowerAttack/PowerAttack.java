39681e681fd5391b8ab7304a3c8d19eff91c1902
liammclay <mclay.liam.24@c-dh.org>
2023-11-30 13:58:34 -0600

    vdvjadvjhafhfvbhefvq

---
+++ 040_PowerAttack/PowerAttack.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class PowerAttack {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Your name is noah macn-something, and kai bodden is your arch nemesis.  To attack kai you use your weapon, your ability to annoy, and point out that kai doesnt have a lanyard to the teacher.  kai isn't that easy tho, he can bs and distract the teacher to a certain degree, so your attack won't always work.");
+
+        int health = 13;
+        int strength = 3;
+
+        while(health >0){
+            System.out.print("would you like to attack kai? ");
+            String choice = scan.nextLine();
+
+            if(choice.equals("yes") || choice.equals("attack")){
+                int attackPower = (int)(Math.random()*10+1);
+                int attack = attackPower+strength;
+
+                System.out.println("you attack with a strength of "+attack);
+                if(attack > 11){
+                    int damage = (int)(Math.random()*3+1);
+                    if(damage == 1){
+                        damage = 4;
+                    }
+                    else if(damage == 2){
+                        damage = 5;
+                    }
+                    else{
+                        damage = 6;
+                    }
+
+                    System.out.println("you dealt "+damage+" damage.  The teacher is getting suspicious of kai.");
+                    health = health - damage;
+                }
+                else{
+                    System.out.println("your attack wasnt string enough, kai talks his way out of this one.");
+                }
+            }
+            else{
+                System.out.println("you should attack kai if you want to win, try again.");
+            }
+        }
+        System.out.println("You did it, the teacher has sent kai to hutch.  Kai now has detention after school, but he'll get his revenge...");
+    }
+    
+}
+/*
+ * made health and attack, made it so if the user attacked they would deal random damgae and if it was enough, theywould injure the enemy once the health hit zero they won
+ */
---
+++ 041_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
@@ -0,0 +1,9 @@
+public class SimpleCounterButAForLoop {
+    public static void main(String[] args) {
+        System.out.println("counting up to 300: ");
+
+        for(i=0; i<=300; i++){
+            System.out.println(i);
+        }
+    }
+}
