f1669cf41f0167851a700d50222b85a69d3281d9
liammclay <mclay.liam.24@c-dh.org>
2023-11-30 14:05:02 -0600

    bvdswq

--- 041_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
+++ 041_SimpleCounterButAForLoop/SimpleCounterButAForLoop.java
@@ -1,8 +1,22 @@
 public class SimpleCounterButAForLoop {
     public static void main(String[] args) {
         System.out.println("counting up to 300: ");
+        for(int i=0; i<=300; i++){
+            System.out.println(i);
+        }
 
-        for(i=0; i<=300; i++){
+        System.out.println("counting down from 300:");
+        for(int i=300; i>=0; i--){
+            System.out.println(i);
+        }
+
+        System.out.println("numbers between 4798 and 5324:");
+        for(int i=4798; i<=5324; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("counting down from 5324 to 4798:");
+        for(int i=5324; i>=4798; i--){
             System.out.println(i);
         }
     }
