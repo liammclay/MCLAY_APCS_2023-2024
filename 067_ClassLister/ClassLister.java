db554f5867930e7cacc98f0e88caa5045fc96d0d
liammclay <mclay.liam.24@c-dh.org>
2024-03-06 14:19:39 -0600

    kai is going to lose this race

---
+++ 067_ClassLister/ClassLister.java
@@ -0,0 +1,9 @@
+public class ClassLister {
+    public static void main(String[] args){
+        String[] classes = {"Physics", "AP calc", "Digital Photrography", "JROTC", "Spectrum", "AP Comp Sci"};
+
+        for(String classNames : classes){
+            System.out.println(classNames);
+        }
+    }
+}
