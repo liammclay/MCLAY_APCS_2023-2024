fa7a53309ab0829791821f80e0f6945f85cc1b64
liammclay <mclay.liam.24@c-dh.org>
2024-03-19 13:42:01 -0500

    nnrgrssrtnwrtndfb

---
+++ RollYourOwnDice/Dice.java
@@ -0,0 +1,15 @@
+public class Dice {
+    private int numSides;
+    private int currentValue;
+
+    public Dice(int numSides){
+        this.numSides = numSides;
+    }
+
+    public int getCurrentvalue(){
+        return currentValue;
+    }
+    public int setCurrentValue(int addValue){
+        return currentValue+addValue;
+    }
+}
