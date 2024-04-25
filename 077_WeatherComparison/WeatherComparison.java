d8e4e06c381d13e5f6e7668416a9c8db9b7df45b
liammclay <mclay.liam.24@c-dh.org>
2024-04-25 14:24:11 -0500

    mujnyhbtgfrde

--- 077_WeatherComparison/WeatherComparison.java
+++ 077_WeatherComparison/WeatherComparison.java
@@ -1,12 +1,17 @@
 public class WeatherComparison {
     public static void main(String[] args) {
-        double[][] aprilWeather[5][6] =
-        {{62.6, 53.9, 34.9, 43.1, 44.1},
-        {36.9, 32.1, 43.8, 33.5, 33.7}, 
-        {51.8, 49.9, 48.8, 52.8, 47.0},
-        {21.6, 28.3, 27.6, 21.1, 28.6},
-        {37.4, 39.1, 40.5, 38.3, 42.0},
-        {45.4, 53.0, 29.6, 31.8, 43.2}};
+        WeatherComparison weather = new WeatherComparison();
+        double[][] aprilWeather =
+        {{62.6, 53.9, 34.9, 43.1, 44.1, 40.4, 39.7},
+        {36.9, 32.1, 43.8, 33.5, 33.7, 38.1, 37.1},
+        {51.8, 49.9, 48.8, 52.8, 47.0, 45.5, 47.5},
+        {21.6, 28.3, 27.6, 21.1, 28.6, 19.0, 22.1},
+        {37.4, 39.1, 40.5, 38.3, 42.0, 48.2, 55.7},
+        {45.4, 53.0, 29.6, 31.8, 43.2, 50.7, 54.9}};
+
+        weather.avgAll(aprilWeather);
+        weather.avgWeek(aprilWeather);
+        weather.avgDay(aprilWeather);
     }
 
     public void avgAll(double[][] arr){
@@ -21,39 +26,40 @@ public class WeatherComparison {
         }
 
         double avg = total/count;
-        System.out.println("The average of all of the temps is, "+avg);
+        System.out.println("The average of all of the temps is, "+avg+" degrees");
     }
 
     public void avgWeek(double[][] arr){
+        int year = 2015;
         for(int row = 0; row<arr.length; row++){
             double total = 0;
             int count = 0;
             double weekAvg = 0;
-            int year = 2015;
-            for(int col = 0; arr[row].length; col++){
+            for(int col = 0; col< arr[row].length; col++){
                 total += arr[row][col];
                 count++;
             }
             weekAvg = total/count;
-            System.out.println("The weeks average in "+year+" is "+weekAvg);
+            System.out.println("The weeks average in "+year+" is "+weekAvg+" degrees");
             year++;
         }
     }
 
     public void avgDay(double[][] arr){
-        int[] avg = new int[arr[0].length];
+        double[] avg = new double[arr[0].length];
         for (int col = 0;  col<arr[0].length; col++){
             double total = 0;
             int count = 0;
-            for(int row = 0; row<arr.lngth; row++){
+            for(int row = 0; row<arr.length; row++){
                 total += arr[row][col];
                 count++;
             }
             double average = total/count;
             avg[col] = average;
         }
+        System.out.println("These are the averages for each day over the 6 years:");
         for(double days:avg){
-            System.out.println();
+            System.out.println(days+" degrees");
         }
     }
 }
